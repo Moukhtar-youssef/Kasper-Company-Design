25d6b322dde506a6e8d5b91c02f3aebbab5e9d9d
Moukhtar-youssef <moukhtar.youssef06@gmail.com>
2022-05-08 15:46:25 +0400

    modifing show more and adding template 🔥

--- js/main.js
+++ js/main.js
@@ -5,11 +5,13 @@ function showmore() {
         hidden_.classList.toggle("hidden");
         document.getElementById("hidden").style.display = "flex";
         hid = true;
+        document.getElementById("more").innerHTML = "show less"
     }
     else {
         var hidden_ = document.querySelector('#hidden');
         hidden_.classList.toggle("hidden");
         document.getElementById("hidden").style.display = "none";
         hid = false;
+        document.getElementById("more").innerHTML = "show more"
     }
 }
